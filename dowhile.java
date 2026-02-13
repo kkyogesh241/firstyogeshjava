af75af3ee09e351e6f31176105c574020b9b91a6
kkyogesh241 <kkyogesh241@gmail.com>
2026-02-13 20:18:35 +0530

    version

---
+++ dowhile.java
@@ -0,0 +1,15 @@
+class dowhile
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        System.out.println("Sum of first 10 even numbers:\n");
+        do
+        {
+            System.out.println(i);
+            i=i+2;
+        }
+        while(i<=10);
+        
+        }
+        }
