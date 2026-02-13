0078b954fbe0490673fa7adf1b79428feb6129eb
kkyogesh241 <kkyogesh241@gmail.com>
2026-02-13 20:11:24 +0530

    version

---
+++ whileloop.java
@@ -0,0 +1,13 @@
+class whileloop
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        System.out.println("Print the sum of first 100 even numbers:\n");
+        while(i<=100)
+    {
+        System.out.println(i);
+        i=i+2;
+    }
+    }
+}
