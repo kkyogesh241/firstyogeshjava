6d6f6221b878de610c60466858bbbe9333f8dd52
kkyogesh241 <kkyogesh241@gmail.com>
2026-02-13 19:56:46 +0530

    version1

---
+++ loopingstatement.java
@@ -0,0 +1,12 @@
+class loopingstatement
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        for(int j=1;j<=10;j++)
+        {
+            sum=sum+j;
+            System.out.println("The sum of first 10 Natural Numbers is:"+sum);
+        }
+    }
+}
