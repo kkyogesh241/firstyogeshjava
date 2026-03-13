80615ee10a3bd4ff72b934f88ecc4b7a413fb367
kkyogesh241 <kkyogesh241@gmail.com>
2026-03-13 14:38:22 +0530

    version

---
+++ evennum.java
@@ -0,0 +1,15 @@
+public class evennum
+{
+    public static void main(String args[])
+    {
+        int i=1,num=100;
+        System.out.println("The Even number from 1 t0"+ num + " is:");
+        for(i=1;i<=num;i++)
+        {
+        if(i%2==0)
+        {
+            System.out.println(i+" ");
+        }
+    }
+}
+}
