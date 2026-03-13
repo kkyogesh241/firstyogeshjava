b32d9d71680cc23d1875a782e04fa78861b394da
kkyogesh241 <kkyogesh241@gmail.com>
2026-03-13 14:06:36 +0530

    version

---
+++ largest.java
@@ -0,0 +1,20 @@
+class largest
+{
+    public static void main(String args[])
+    {
+        int a=10, b=15, c=20;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("A is greater than B and C");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println("B is greater than C and A");
+        }
+        else
+        {
+            System.out.println("C is greater than all");
+        }
+        }
+        }
+    
